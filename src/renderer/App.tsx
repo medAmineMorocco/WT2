1f09afb414598fc111e74213854447f7d8d108c8
medamine <mohamedamineammach@gmail.com>
2024-02-12 22:31:00 +0100

    add dark theme

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,37 +1,59 @@
 import React, { useState } from 'react';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import { Layout, theme } from 'antd';
+import { ConfigProvider, Layout, theme, Switch } from 'antd';
 
 const { Content, Sider } = Layout;
+const { defaultAlgorithm, darkAlgorithm } = theme;
 
 function Hello() {
   const [collapsed, setCollapsed] = useState(false);
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const onChange = () => {
+    setIsDarkMode((previousValue) => !previousValue);
+  };
+
   return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Sider
-        collapsible
-        collapsed={collapsed}
-        onCollapse={(value) => setCollapsed(value)}
-      />
-      <Layout>
-        <Content style={{ margin: '16px' }}>
-          <div
-            style={{
-              padding: 24,
-              minHeight: 360,
-              background: colorBgContainer,
-              borderRadius: borderRadiusLG,
-            }}
-          >
-            Bill is a cat.
+    <ConfigProvider
+      theme={{ algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm }}
+    >
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider
+          theme="light"
+          collapsible
+          collapsed={collapsed}
+          onCollapse={(value) => setCollapsed(value)}
+        >
+          <div style={{ marginLeft: '8px', marginTop: '8px' }}>
+            <Switch
+              defaultChecked
+              onChange={onChange}
+              checkedChildren="Light"
+              unCheckedChildren="Dark"
+            />
           </div>
-        </Content>
+        </Sider>
+        <Layout>
+          <Content style={{ margin: '16px' }}>
+            <div
+              style={{
+                padding: 24,
+                minHeight: 360,
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG,
+              }}
+            >
+              Bill is a cat.
+            </div>
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </ConfigProvider>
   );
 }
 
