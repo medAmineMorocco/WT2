f46e1f2e6113aa3f8f6515c6da1ea534eb5f744a
medamine <mohamedamineammach@gmail.com>
2024-02-12 22:00:58 +0100

    layout app

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,9 +1,38 @@
+import React, { useState } from 'react';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import { DatePicker } from 'antd';
+import { Layout, theme } from 'antd';
+
+const { Content, Sider } = Layout;
 
 function Hello() {
-  return <DatePicker />;
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider
+        collapsible
+        collapsed={collapsed}
+        onCollapse={(value) => setCollapsed(value)}
+      />
+      <Layout>
+        <Content style={{ margin: '16px' }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            Bill is a cat.
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
+  );
 }
 
 export default function App() {
