25dc32e4eac4bf34bc1fddb43320873b28e920bf
medamine <mohamedamineammach@gmail.com>
2024-02-13 18:28:39 +0100

    add basic ui for worktrees section

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,21 +1,84 @@
 import React, { useState } from 'react';
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import { ConfigProvider, Layout, theme, Switch } from 'antd';
+import {
+  ConfigProvider,
+  Layout,
+  theme,
+  Switch,
+  Divider,
+  Modal,
+  Form,
+  Input,
+  Button,
+  Dropdown,
+  Tooltip,
+  MenuProps,
+} from 'antd';
+import {
+  FolderAddOutlined,
+  MoreOutlined,
+  DeleteOutlined,
+  SunOutlined,
+  MoonOutlined,
+  ExclamationCircleFilled,
+  CheckOutlined,
+} from '@ant-design/icons';
 
 const { Content, Sider } = Layout;
 const { defaultAlgorithm, darkAlgorithm } = theme;
+const { confirm } = Modal;
+const { useToken } = theme;
 
 function Hello() {
   const [collapsed, setCollapsed] = useState(false);
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
+  const { token } = useToken();
 
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   const onChange = () => {
     setIsDarkMode((previousValue) => !previousValue);
+  };
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const [form] = Form.useForm();
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  const items = [
+    {
+      label: 'Delete',
+      key: '0',
+      icon: <DeleteOutlined />,
+      danger: true,
+    },
+  ];
+
+  const onClick: MenuProps['onClick'] = () => {
+    confirm({
+      title: 'Are you sure delete this worktree ?',
+      icon: <ExclamationCircleFilled />,
+      okText: 'Yes',
+      okType: 'danger',
+      cancelText: 'No',
+      centered: true,
+      onOk() {
+        console.log('OK');
+      },
+      onCancel() {
+        console.log('Cancel');
+      },
+    });
   };
 
   return (
@@ -33,10 +96,97 @@ function Hello() {
             <Switch
               defaultChecked
               onChange={onChange}
-              checkedChildren="Light"
-              unCheckedChildren="Dark"
+              checkedChildren={<SunOutlined />}
+              unCheckedChildren={<MoonOutlined />}
             />
           </div>
+          {!collapsed && (
+            <Divider orientation="left">
+              Worktrees{' '}
+              <Tooltip title="Add new worktree" placement="right">
+                <FolderAddOutlined
+                  style={{ cursor: 'pointer' }}
+                  onClick={showModal}
+                />
+              </Tooltip>
+              <Modal
+                open={isModalOpen}
+                centered
+                footer={null}
+                onCancel={handleCancel}
+                destroyOnClose
+                width={400}
+              >
+                <Form layout="inline" form={form}>
+                  <Form.Item
+                    label="Name"
+                    name="name"
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Please input your worktree name!',
+                      },
+                    ]}
+                  >
+                    <Input />
+                  </Form.Item>
+                  <Form.Item>
+                    <Button
+                      type="primary"
+                      htmlType="submit"
+                      icon={<CheckOutlined />}
+                    />
+                  </Form.Item>
+                </Form>
+              </Modal>
+            </Divider>
+          )}
+          {!collapsed && (
+            <div>
+              <ul style={{ paddingLeft: '8px' }}>
+                <li
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    height: '24px',
+                    color: isDarkMode ? token.colorBgBase : token.colorTextBase,
+                  }}
+                >
+                  worktree1
+                  <Dropdown
+                    menu={{ items, onClick }}
+                    trigger={['click']}
+                    placement="bottomRight"
+                  >
+                    <Tooltip title="Worktree actions" placement="right">
+                      <MoreOutlined style={{ cursor: 'pointer' }} />
+                    </Tooltip>
+                  </Dropdown>
+                </li>
+                <li
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    height: '24px',
+                    color: isDarkMode ? token.colorBgBase : token.colorTextBase,
+                  }}
+                >
+                  worktree2
+                  <Dropdown
+                    menu={{ items, onClick }}
+                    trigger={['click']}
+                    placement="bottomRight"
+                  >
+                    <Tooltip title="Worktree actions" placement="right">
+                      <MoreOutlined style={{ cursor: 'pointer' }} />
+                    </Tooltip>
+                  </Dropdown>
+                </li>
+              </ul>
+            </div>
+          )}
         </Sider>
         <Layout>
           <Content style={{ margin: '8px' }}>
