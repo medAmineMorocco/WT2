b1105e45aa32cb8dde9a767a34a6054117791100
medamine <mohamedamineammach@gmail.com>
2024-02-15 14:45:01 +0100

    add icons actions to log section

--- src/renderer/modules/execution/Execution.tsx
+++ src/renderer/modules/execution/Execution.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
-import { theme, Space } from 'antd';
-import { FileOutlined, BlockOutlined } from '@ant-design/icons';
+import React, { useState } from 'react';
+import { theme, Space, Tooltip } from 'antd';
+import {
+  FileOutlined,
+  BlockOutlined,
+  CopyOutlined,
+  ExpandOutlined,
+  CheckOutlined,
+} from '@ant-design/icons';
 
 const { useToken } = theme;
 
@@ -10,6 +16,15 @@ export default function Execution() {
   } = theme.useToken();
 
   const { token } = useToken();
+
+  const [isCopied, setCopied] = useState(false);
+
+  const onCopyClick = () => {
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  };
 
   return (
     <div style={{ display: 'flex', gap: '8px' }}>
@@ -30,6 +45,7 @@ export default function Execution() {
       </div>
       <div
         style={{
+          position: 'relative',
           flexGrow: 1,
           padding: 12,
           height: '47vh',
@@ -42,6 +58,24 @@ export default function Execution() {
           <FileOutlined />
           <strong>Log</strong>
         </Space>
+
+        <div style={{ position: 'absolute', top: '8px', right: '8px' }}>
+          <Space>
+            <Tooltip title={!isCopied ? 'Copy' : 'Copied!'} placement="left">
+              {!isCopied ? (
+                <CopyOutlined
+                  style={{ cursor: 'pointer' }}
+                  onClick={onCopyClick}
+                />
+              ) : (
+                <CheckOutlined />
+              )}
+            </Tooltip>
+            <Tooltip title="Enter fullscreen mode" placement="left">
+              <ExpandOutlined style={{ cursor: 'pointer' }} />
+            </Tooltip>
+          </Space>
+        </div>
       </div>
     </div>
   );
