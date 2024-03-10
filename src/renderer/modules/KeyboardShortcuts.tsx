60492e785bf945b0226813150c2226c4b6157c38
medamine <mohamedamineammach@gmail.com>
2024-03-10 14:05:30 +0000

    add tabs in keyboard shortcuts

--- src/renderer/modules/KeyboardShortcuts.tsx
+++ src/renderer/modules/KeyboardShortcuts.tsx
@@ -1,69 +1,72 @@
 import React from 'react';
-import { List, Modal, Space, Typography } from 'antd';
+import { List, Modal, Space, Typography, Tabs } from 'antd';
 
 const { Text } = Typography;
-const data = [
-  {
-    title: 'Menu',
-    shortcut1: 'Shift',
-    shortcut2: 'M',
-  },
-  {
-    title: 'Open Repository',
-    shortcut1: 'Shift',
-    shortcut2: 'O',
-  },
-  {
-    title: 'Add Worktree',
-    shortcut1: 'Shift',
-    shortcut2: 'W',
-  },
-  {
-    title: 'Add Workflow',
-    shortcut1: 'Shift',
-    shortcut2: 'A',
-  },
-  {
-    title: 'Import Workflow',
-    shortcut1: 'Shift',
-    shortcut2: 'I',
-  },
-  {
-    title: 'Toggle Fullscreen',
-    shortcut1: 'Shift',
-    shortcut2: 'S',
-  },
-  {
-    title: 'Copy Log',
-    shortcut1: 'Shift',
-    shortcut2: 'L',
-  },
-  {
-    title: 'Toggle theme',
-    shortcut1: 'Shift',
-    shortcut2: 'T',
-  },
-  {
-    title: 'Collapse Sidebar',
-    shortcut1: 'Shift',
-    shortcut2: 'C',
-  },
-  {
-    title: 'New tab',
-    shortcut1: 'Shift',
-    shortcut2: 'N',
-  },
-  {
-    title: 'Move to next tab',
-    shortcut1: 'Shift',
-    shortcut2: '→',
-  },
-  {
-    title: 'Move to previous tab',
-    shortcut1: 'shift',
-    shortcut2: '←',
-  },
-];
+const data = {
+  general: [
+    {
+      title: 'Menu',
+      shortcut1: 'Shift',
+      shortcut2: 'M',
+    },
+    {
+      title: 'Toggle theme',
+      shortcut1: 'Shift',
+      shortcut2: 'T',
+    },
+    {
+      title: 'Collapse Sidebar',
+      shortcut1: 'Shift',
+      shortcut2: 'C',
+    },
+  ],
+  tabs: [
+    {
+      title: 'New tab',
+      shortcut1: 'Shift',
+      shortcut2: 'N',
+    },
+    {
+      title: 'Move to next tab',
+      shortcut1: 'Shift',
+      shortcut2: '→',
+    },
+    {
+      title: 'Move to previous tab',
+      shortcut1: 'shift',
+      shortcut2: '←',
+    },
+  ],
+  worktree: [
+    {
+      title: 'Add Worktree',
+      shortcut1: 'Shift',
+      shortcut2: 'W',
+    },
+  ],
+  workflow: [
+    {
+      title: 'Add Workflow',
+      shortcut1: 'Shift',
+      shortcut2: 'A',
+    },
+    {
+      title: 'Import Workflow',
+      shortcut1: 'Shift',
+      shortcut2: 'I',
+    },
+    {
+      title: 'Toggle Fullscreen',
+      shortcut1: 'Shift',
+      shortcut2: 'S',
+    },
+    {
+      title: 'Copy Log',
+      shortcut1: 'Shift',
+      shortcut2: 'L',
+    },
+  ],
+};
 export default function KeyboardShortcuts({
   open,
   onClose,
@@ -89,18 +92,86 @@ export default function KeyboardShortcuts({
       destroyOnClose
       footer={null}
     >
-      <List
-        grid={{ gutter: 16, column: 2 }}
-        dataSource={data}
-        renderItem={(item) => (
-          <List.Item>
-            <div style={{ display: 'flex' }}>
-              <span style={{ flex: 1 }}>{item.title}</span>{' '}
-              <Text keyboard>{item.shortcut1}</Text>
-              <Text keyboard>{item.shortcut2}</Text>
-            </div>
-          </List.Item>
-        )}
+      <Tabs
+        tabPosition="left"
+        items={[
+          {
+            label: 'General',
+            key: '10',
+            children: (
+              <List
+                grid={{ column: 1 }}
+                dataSource={data.general}
+                renderItem={(item) => (
+                  <List.Item>
+                    <div style={{ display: 'flex' }}>
+                      <span style={{ flex: 1 }}>{item.title}</span>{' '}
+                      <Text keyboard>{item.shortcut1}</Text>
+                      <Text keyboard>{item.shortcut2}</Text>
+                    </div>
+                  </List.Item>
+                )}
+              />
+            ),
+          },
+          {
+            label: 'Tabs',
+            key: '20',
+            children: (
+              <List
+                grid={{ column: 1 }}
+                dataSource={data.tabs}
+                renderItem={(item) => (
+                  <List.Item>
+                    <div style={{ display: 'flex' }}>
+                      <span style={{ flex: 1 }}>{item.title}</span>{' '}
+                      <Text keyboard>{item.shortcut1}</Text>
+                      <Text keyboard>{item.shortcut2}</Text>
+                    </div>
+                  </List.Item>
+                )}
+              />
+            ),
+          },
+          {
+            label: 'Worktree',
+            key: '30',
+            children: (
+              <List
+                grid={{ column: 1 }}
+                dataSource={data.worktree}
+                renderItem={(item) => (
+                  <List.Item>
+                    <div style={{ display: 'flex' }}>
+                      <span style={{ flex: 1 }}>{item.title}</span>{' '}
+                      <Text keyboard>{item.shortcut1}</Text>
+                      <Text keyboard>{item.shortcut2}</Text>
+                    </div>
+                  </List.Item>
+                )}
+              />
+            ),
+          },
+          {
+            label: 'Workflow',
+            key: '40',
+            children: (
+              <List
+                grid={{ column: 1 }}
+                dataSource={data.workflow}
+                renderItem={(item) => (
+                  <List.Item>
+                    <div style={{ display: 'flex' }}>
+                      <span style={{ flex: 1 }}>{item.title}</span>{' '}
+                      <Text keyboard>{item.shortcut1}</Text>
+                      <Text keyboard>{item.shortcut2}</Text>
+                    </div>
+                  </List.Item>
+                )}
+              />
+            ),
+          },
+        ]}
       />
     </Modal>
   );
