0722cac84dd9ab8904eb777de371b9da2c9aa6eb
medamine <mohamedamineammach@gmail.com>
2024-04-12 18:08:22 +0000

    save notifications settings state

--- src/renderer/modules/settings/NotificationSettings.tsx
+++ src/renderer/modules/settings/NotificationSettings.tsx
@@ -1,6 +1,17 @@
 import { Form, Switch } from 'antd';
+import { useEffect, useState } from 'react';
 
 export default function NotificationSettings() {
+  const [enabled, setEnabled] = useState(true);
+
+  useEffect(() => {
+    setEnabled(window.localStorage.getItem('notificationsEnabled') === 'true');
+  }, []);
+  const onChange = (checked: boolean) => {
+    window.localStorage.setItem('notificationsEnabled', checked.toString());
+    setEnabled(checked);
+  };
+
   return (
     <Form
       layout="horizontal"
@@ -8,8 +19,12 @@ export default function NotificationSettings() {
       labelCol={{ span: 12 }}
       wrapperCol={{ span: 8 }}
     >
-      <Form.Item label="Enable Desktop Notifications" valuePropName="checked">
-        <Switch />
+      <Form.Item
+        label="Enable Desktop Notifications"
+        name="notificationsEnabled"
+        valuePropName="checked"
+      >
+        <Switch onChange={onChange} value={enabled} />
       </Form.Item>
     </Form>
   );
