27ed6b3a5b3040801da0160c39e8d73f071d58ab
medamine <mohamedamineammach@gmail.com>
2024-05-05 12:55:36 +0100

    calculate cols and rows for terminal

--- src/renderer/components/terminal/TerminalUI.tsx
+++ src/renderer/components/terminal/TerminalUI.tsx
@@ -15,6 +15,23 @@ export default function TerminalUI({
   const terminalRef = useRef(null);
   const { isDarkMode } = useItemsContext();
 
+  function calculateColsRows() {
+    const terminalElement = terminalRef.current;
+
+    // Get the dimensions of the terminal container
+    const containerWidth = terminalElement.parentElement?.clientWidth;
+    const containerHeight = terminalElement.parentElement?.clientHeight;
+
+    // Get the dimensions of a single character in the terminal
+    const charWidth = terminalElement.offsetWidth / terminal.cols;
+    const charHeight = terminalElement.offsetHeight / terminal.rows;
+
+    // Calculate the number of columns and rows
+    const cols = Math.floor(containerWidth / charWidth);
+    const rows = Math.floor(containerHeight / charHeight);
+    return { cols, rows };
+  }
+
   useEffect(() => {
     terminal = new Terminal({
       convertEol: true,
@@ -35,13 +52,38 @@ export default function TerminalUI({
         terminal.scrollToTop();
       });
     }
+
+    function handleResize() {
+      if (terminal && terminalRef.current) {
+        const { cols, rows } = calculateColsRows();
+        terminal.resize(cols, rows);
+        fitAddon.fit();
+      }
+    }
+
+    window.addEventListener('resize', handleResize);
+
     return () => {
       terminal.dispose();
       if (terminalRef.current) {
         terminalRef.current.innerHTML = '';
       }
+      window.removeEventListener('resize', handleResize);
     };
   }, [backgroundDarkMode, isDarkMode, output]);
 
-  return <div ref={terminalRef} style={{ width: '100%', height: '100%' }} />;
+  useEffect(() => {
+    const { cols, rows } = calculateColsRows();
+    terminal.resize(cols, rows);
+  }, [terminal]);
+
+  return (
+    <div
+      ref={terminalRef}
+      style={{
+        width: '100%',
+        height: '100%',
+      }}
+    />
+  );
 }
