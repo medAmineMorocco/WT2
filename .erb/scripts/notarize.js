3ece2e4b88b54f8ba6cd92565627cf6af95f50e6
medamine <mohamedamineammach@gmail.com>
2026-01-30 13:53:15 +0100

    notarize worktreewise on mac

--- .erb/scripts/notarize.js
+++ .erb/scripts/notarize.js
@@ -3,6 +3,7 @@ const { build } = require('../../package.json');
 
 exports.default = async function notarizeMacos(context) {
   const { electronPlatformName, appOutDir } = context;
+
   if (electronPlatformName !== 'darwin') {
     return;
   }
@@ -13,20 +14,31 @@ exports.default = async function notarizeMacos(context) {
   }
 
   if (
-    !('APPLE_ID' in process.env && 'APPLE_APP_SPECIFIC_PASSWORD' in process.env)
+    !process.env.APPLE_ID ||
+    !process.env.APPLE_APP_SPECIFIC_PASSWORD ||
+    !process.env.APPLE_TEAM_ID
   ) {
     console.warn(
-      'Skipping notarizing step. APPLE_ID and APPLE_APP_SPECIFIC_PASSWORD env variables must be set',
+      'Skipping notarizing step. APPLE_ID, APPLE_APP_SPECIFIC_PASSWORD and APPLE_TEAM_ID must be set'
     );
     return;
   }
 
   const appName = context.packager.appInfo.productFilename;
 
-  await notarize({
-    appBundleId: build.appId,
-    appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLE_ID,
-    appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
-  });
+  console.log('>>> Notarization started');
+
+  try {
+    await notarize({
+      tool: 'notarytool',
+      appBundleId: build.appId,
+      appPath: `${appOutDir}/${appName}.app`,
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
+      teamId: process.env.APPLE_TEAM_ID,
+    });
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 };
